1667147f0e919c5d20697887cdd7e59232eed3e4
Odin Asbjørnsen <odinas004@gmail.com>
2023-10-06 15:34:49 +0200

    Refactor writing color kotlin file (#68)

--- src/main/kotlin/FileUtils.kt
+++ src/main/kotlin/FileUtils.kt
@@ -1,8 +1,10 @@
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import java.io.File
 import java.io.FileNotFoundException
 import java.nio.file.FileAlreadyExistsException
+import java.util.Locale
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileSystemView
 import kotlin.io.path.Path
@@ -39,63 +41,11 @@ internal fun saveColorsToFile(
         val path = file.path
         val outputFile = Path(path, "colors.kt").createFile().toFile()
         outputFile.apply {
-            appendText("val Primary: Color = Color(0x${currentColorPalette.primary.hexCode.takeLast(8)}) \n")
-            appendText("val OnPrimary: Color = Color(0x${currentColorPalette.onPrimary.hexCode.takeLast(8)}) \n")
-            appendText("val PrimaryContainer: Color = Color(0x${currentColorPalette.primaryContainer.hexCode.takeLast(8)}) \n")
-            appendText(
-                "val OnPrimaryContainer: Color = Color(0x${
-                    currentColorPalette.onPrimaryContainer.hexCode.takeLast(
-                        8
-                    )
-                }) \n"
-            )
-            appendText("val InversePrimary: Color = Color(0x${currentColorPalette.inversePrimary.hexCode.takeLast(8)}) \n")
-            appendText("val Secondary: Color = Color(0x${currentColorPalette.secondary.hexCode.takeLast(8)}) \n")
-            appendText("val OnSecondary: Color = Color(0x${currentColorPalette.onSecondary.hexCode.takeLast(8)}) \n")
-            appendText(
-                "val SecondaryContainer: Color = Color(0x${
-                    currentColorPalette.secondaryContainer.hexCode.takeLast(
-                        8
-                    )
-                }) \n"
-            )
-            appendText(
-                "val OnSecondaryContainer: Color = Color(0x${
-                    currentColorPalette.onSecondaryContainer.hexCode.takeLast(
-                        8
-                    )
-                }) \n"
-            )
-            appendText("val Tertiary: Color = Color(0x${currentColorPalette.tertiary.hexCode.takeLast(8)}) \n")
-            appendText("val OnTertiary: Color = Color(0x${currentColorPalette.onTertiary.hexCode.takeLast(8)}) \n")
-            appendText(
-                "val TertiaryContainer: Color = Color(0x${
-                    currentColorPalette.tertiaryContainer.hexCode.takeLast(
-                        8
-                    )
-                }) \n"
-            )
-            appendText(
-                "val OnTertiaryContainer: Color = Color(0x${
-                    currentColorPalette.onTertiaryContainer.hexCode.takeLast(
-                        8
-                    )
-                }) \n"
-            )
-            appendText("val Background: Color = Color(0x${currentColorPalette.background.hexCode.takeLast(8)}) \n")
-            appendText("val OnBackground: Color = Color(0x${currentColorPalette.onBackground.hexCode.takeLast(8)}) \n")
-            appendText("val Surface: Color = Color(0x${currentColorPalette.surface.hexCode.takeLast(8)}) \n")
-            appendText("val OnSurface: Color = Color(0x${currentColorPalette.onSurface.hexCode.takeLast(8)}) \n")
-            appendText("val SurfaceVariant: Color = Color(0x${currentColorPalette.surfaceVariant.hexCode.takeLast(8)}) \n")
-            appendText("val OnSurfaceVariant: Color = Color(0x${currentColorPalette.onSurfaceVariant.hexCode.takeLast(8)}) \n")
-            appendText("val SurfaceTint: Color = Color(0x${currentColorPalette.surfaceTint.hexCode.takeLast(8)}) \n")
-            appendText("val InverseSurface: Color = Color(0x${currentColorPalette.inverseSurface.hexCode.takeLast(8)}) \n")
-            appendText("val InverseOnSurface: Color = Color(0x${currentColorPalette.inverseOnSurface.hexCode.takeLast(8)}) \n")
-            appendText("val Error: Color = Color(0x${currentColorPalette.error.hexCode.takeLast(8)}) \n")
-            appendText("val OnError: Color = Color(0x${currentColorPalette.onError.hexCode.takeLast(8)}) \n")
-            appendText("val ErrorContainer: Color = Color(0x${currentColorPalette.errorContainer.hexCode.takeLast(8)}) \n")
-            appendText("val OnErrorContainer: Color = Color(0x${currentColorPalette.onErrorContainer.hexCode.takeLast(8)}) \n")
-            appendText("val Outline: Color = Color(0x${currentColorPalette.outline.hexCode.takeLast(8)}) \n")
+
+            getColorList(colorPalette = currentColorPalette).forEach { color ->
+                appendText(getColorName(colorNameLowerCase = color.key, color = color.value))
+            }
+
         }
         onResult(true, "Saving colors worked")
     } catch (fileNotFound: FileNotFoundException) {
@@ -105,4 +55,48 @@ internal fun saveColorsToFile(
     } catch (fileAlreadyExists: FileAlreadyExistsException) {
         onResult(false, "A file with this name already exists here.")
     }
+}
+
+
+internal fun getColorName(colorNameLowerCase: String, color: Color): String {
+    val capitalizedColorName = colorNameLowerCase.replaceFirstChar {
+        if (it.isLowerCase()) {
+            it.titlecase(Locale.getDefault())
+        } else {
+            it.toString()
+        }
+    }
+    return "val $capitalizedColorName: Color = Color(0x${color.hexCode.takeLast(8)}) \n"
+}
+
+private fun getColorList(colorPalette: ColorScheme): Map<String, Color> {
+    return mapOf(
+        "primary" to colorPalette.primary,
+        "onPrimary" to colorPalette.onPrimary,
+        "primaryContainer" to colorPalette.primaryContainer,
+        "onPrimaryContainer" to colorPalette.onPrimaryContainer,
+        "inversePrimary" to colorPalette.inversePrimary,
+        "secondary" to colorPalette.secondary,
+        "onSecondary" to colorPalette.onSecondary,
+        "secondaryContainer" to colorPalette.secondaryContainer,
+        "onSecondaryContainer" to colorPalette.onSecondaryContainer,
+        "tertiary" to colorPalette.tertiary,
+        "onTertiary" to colorPalette.onTertiary,
+        "tertiaryContainer" to colorPalette.tertiaryContainer,
+        "onTertiaryContainer" to colorPalette.onTertiaryContainer,
+        "background" to colorPalette.background,
+        "onBackground" to colorPalette.onBackground,
+        "surface" to colorPalette.surface,
+        "onSurface" to colorPalette.onSurface,
+        "surfaceVariant" to colorPalette.surfaceVariant,
+        "onSurfaceVariant" to colorPalette.onSurfaceVariant,
+        "surfaceTint" to colorPalette.surfaceTint,
+        "inverseSurface" to colorPalette.inverseSurface,
+        "inverseOnSurface" to colorPalette.inverseOnSurface,
+        "error" to colorPalette.error,
+        "onError" to colorPalette.onError,
+        "errorContainer" to colorPalette.errorContainer,
+        "onErrorContainer" to colorPalette.onErrorContainer,
+        "outline" to colorPalette.outline,
+    )
 }
