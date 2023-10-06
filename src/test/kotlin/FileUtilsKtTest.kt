c40ef512a17ae8914a892054625d42d27e5e5448
Odin Asbjørnsen <odinas004@gmail.com>
2023-10-06 14:55:38 +0200

    Refactor writing color kotlin file

---
+++ src/test/kotlin/FileUtilsKtTest.kt
@@ -0,0 +1,17 @@
+import androidx.compose.ui.graphics.Color
+import org.junit.Assert.*
+import org.junit.Test
+
+class FileUtilsKtTest {
+    @Test
+    fun testGetColorNameFunction() {
+        val colorNameLowerCase = "red"
+        val color = Color.Red
+
+        val result = getColorName(colorNameLowerCase, color)
+
+        // Assert
+        val expected = "val Red: Color = Color(0xFFFF0000) \n"
+        assertEquals(expected, result)
+    }
+}
